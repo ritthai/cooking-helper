f9c7203f4e382f97857fe285c54b25d060e38f13
Ritchie Thai <ritchiethai@Ritchies-MacBook-Pro.local>
2013-03-17 20:25:18 -0300

    Moved ingredients to their own file and added some ingredients. Added .gitignore for .DS_Store.

---
+++ js/ingredients.js
@@ -0,0 +1,72 @@
+var ingredientsInfo = 
+{
+	'ingredients':
+		[
+			{
+				name: 'rice',
+				caloriesPerCup: 242,
+				isCooked: true,
+				volumeCookedPerUncooked: 2,
+				foodGroup: foodGroups.grain
+			},
+			{
+				name: 'onion',
+				caloriesPerCup: 16,
+				caloriesPerUnit: 64,
+				defaultUnitOfMeasurement: 'unit',
+				foodGroup: foodGroups.vegetable
+			},
+			{
+				name: 'carrot',
+				caloriesPerCup: 52,
+				foodGroup: foodGroups.vegetable
+			},
+			{
+				name: 'broccoli',
+				caloriesPerCup: 25,
+				foodGroup: foodGroups.vegetable
+			},
+			{
+				name: 'cabbage',
+				caloriesPerCup: 22,
+				foodGroup: foodGroups.vegetable
+			},
+			{
+				name: 'frozen brussel sprouts',
+				caloriesPerUnit: 7.5,
+				foodGroup: foodGroups.vegetable
+			},
+			{
+				name: 'frozen peas',
+				caloriesPerCup: 105,
+				foodGroup: foodGroups.vegetable
+			},
+			{
+				name: 'green beans',
+				caloriesPerCup: 30,
+				foodGroup: foodGroups.vegetable
+			},
+			{
+				name: 'green onion',
+				caloriesPerCup: 40,
+				defaultUnitOfMeasurement: '1/4 cup',
+				foodGroup: foodGroups.vegetable
+			},
+			{
+				name: 'ginger',
+				caloriesPerCup: 76,
+				defaultUnitOfMeasurement: '1/4 cup',
+				foodGroup: foodGroups.vegetable
+			},
+			{
+				name: 'poached egg',
+				caloriesPerUnit: 71,
+				foodGroup: foodGroups.meat
+			},
+			{
+				name: 'other calories',
+				caloriesPerUnit: 1,
+				foodGroup: foodGroups.other
+			}
+		]
+};
